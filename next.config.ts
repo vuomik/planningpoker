1adff5e317d0f7068695aa6a6dc82c17314e9cfe
Mike Vuoncino <mike.vuoncino@gmail.com>
2026-05-08 22:18:04 -0400

    Inject vars into build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    PUSHER_APP_ID: process.env.PUSHER_APP_ID || '',
+    PUSHER_KEY: process.env.PUSHER_KEY || '',
+    PUSHER_SECRET: process.env.PUSHER_SECRET || '',
+    PUSHER_CLUSTER: process.env.PUSHER_CLUSTER || 'us2',
+  }
 };
 
 export default nextConfig;
